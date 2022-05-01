e1f28991b0577e032371cac167e60f96915adf58
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2022-05-01 18:16:00 +0200

    Let the user input a number

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const screen = document.querySelector('.screen');
+const numericButtons = document.querySelectorAll('.btn-num');
+
+let currentInput = '';
+
+function init() {
+    numericButtons.forEach(button => {
+        button.addEventListener('click', appendInput);
+    });
+}
+
 function operate(operator, a, b) {
     switch (operator) {
         case 'add':
@@ -28,3 +39,18 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function appendInput(event) {
+    // Allow only one decimal point
+    if (event.target.textContent === '.') {
+        if (currentInput.includes('.')) return;
+    }
+    currentInput += event.target.textContent;
+    updateScreen();
+}
+
+function updateScreen() {
+    screen.textContent = currentInput;
+}
+
+init();
